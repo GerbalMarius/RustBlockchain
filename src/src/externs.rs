f3485336e4e1170333da474fc734254bb374b3e1
Marius Ambrazevičius <mariukas.ambrazevicius@gmail.com>
2024-05-08 00:21:07 +0300

    Added transaction logic.

--- src/src/externs.rs
+++ src/src/externs.rs
@@ -2,3 +2,4 @@ pub mod block;
 pub mod lib;
 pub mod hashable;
 pub mod blockchain;
+pub mod transaction;
--- src/src/externs/block.rs
+++ src/src/externs/block.rs
@@ -2,7 +2,7 @@
 
 use std::fmt::Debug;
 
-use super::{hashable::Hashable, lib::{difficulty_bytes_as_u128, u128_bytes, u32_bytes, u64_bytes, BlockHash}};
+use super::{hashable::Hashable, lib::{difficulty_bytes_as_u128, u128_bytes, u32_bytes, u64_bytes, BlockHash}, transaction::{self, Transaction}};
 
 pub struct Block{
     pub index : u32,
@@ -10,27 +10,27 @@ pub struct Block{
     pub hash : BlockHash,
     pub prev_block_hash : BlockHash,
     pub nonce : u64,
-    pub payload : String,
+    pub transactions:Vec<Transaction>,
     pub difficulty : u128
 }
 
 impl Debug for Block {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Block[{}] : {} at : {} with : {} nonce : {}", &self.index, &hex::encode(&self.hash), &self.timestamp, &self.payload
+        write!(f, "Block[{}] : {} at : {} with : {} nonce : {}", &self.index, &hex::encode(&self.hash), &self.timestamp, &self.transactions.len()
         ,&self.nonce)
     }
 }
 
 impl Block {
     //constructs a new block instance
-    pub fn new( index : u32, timestamp : u128, prev_block_hash : BlockHash, nonce : u64, payload : String, difficulty : u128) ->Self {
+    pub fn new( index : u32, timestamp : u128, prev_block_hash : BlockHash, transactions:Vec<Transaction>,  difficulty : u128) ->Self {
         Block { 
              index,
              timestamp, 
              hash : vec![0;32], 
              prev_block_hash, 
-             nonce, 
-             payload,
+             nonce : 0, 
+             transactions,
              difficulty
              }
     }
@@ -58,7 +58,12 @@ impl Hashable for Block {
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
 
-        bytes.extend(self.payload.as_bytes());
+        bytes.extend(
+            self.transactions
+                .iter()
+                .flat_map(|transaction| transaction.bytes())
+                .collect::<Vec<u8>>()
+        );
         bytes.extend(&u128_bytes(&self.difficulty));
 
         bytes
